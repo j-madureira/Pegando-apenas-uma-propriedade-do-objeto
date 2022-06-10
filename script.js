24349e8721537fa878a18432b7ba7b6b2fdeea61
João Vitor Madureira <103207658+j-madureira@users.noreply.github.com>
2022-06-10 18:33:11 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+//Objeto com três propriedades
+
+let pessoaInfo = {
+    nome:"João",
+    idade:20,
+    cpf:"39012321232",
+    telefone:"82138291312"
+}
+
+
+//Pegando apenas uma propriedade do meu objeto
+let {cpf} = pessoaInfo;
+
+console.log(cpf);
